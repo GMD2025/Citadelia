6ea4738f2a91ff844195bbf396282be555d0ff4c
Sevastian Bahynskyi <baginski.play@gmail.com>
2025-03-14 16:18:46 +0100

    add: custom button editor, start: tilemap

---
+++ Assets/1_Scripts/CustomInspector/Button/InspectorButtonAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _1_Scripts.CustomInspector.Button
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class InspectorButtonAttribute : Attribute
+    {
+        public string ButtonName { get; }
+    
+        public InspectorButtonAttribute(string buttonName)
+        {
+            ButtonName = buttonName;
+        }
+    }
+}
---
+++ Assets/1_Scripts/Tilemap/HighlightTile.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace _1_Scripts.Tilemap
+{
+    public class HighlightTile : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
