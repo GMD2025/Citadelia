488f3eccaea8d794465f022662d73288c16ba6ce
Sevastian Bahynskyi <baginski.play@gmail.com>
2025-03-15 23:17:38 +0100

    remove: duplicate sprite tiles, smal refactors

--- Assets/_Scripts/FingerTracking.cs
+++ Assets/_Scripts/FingerTracking.cs
@@ -13,10 +13,12 @@ public class FingerTracking : MonoBehaviour, IPointerDownHandler, IPointerUpHand
 
         if (isTracking)
         {
-            // mobile
+            // TODO: maybe instead of old input system it is better to use the newer one?
+            // then you just create an action in "InputSystem Actions" and just use Pointer which handles both Mouse and Touch
             if (Input.touchCount > 0)
             {
                 touchPos = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+                // TODO: is better to persist camera and don't search for it every update loop iteration
                 Debug.Log("Finger Position: " + touchPos);
             }
             // computer 
@@ -57,6 +59,7 @@ public class FingerTracking : MonoBehaviour, IPointerDownHandler, IPointerUpHand
     void HighlightTile(GameObject tile)
     {
         tile.GetComponent<SpriteRenderer>().color = Color.yellow; // Change highlight effect as needed
+        // TODO: it is better to persist (cache) components and don't search for them every update loop iteration
     }
 
     void ResetPreviousHighlight()
