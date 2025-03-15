048f15131dfe2c99e1922d3e127e7fb4d1d1288f
Sevastian Bahynskyi <baginski.play@gmail.com>
2025-03-15 16:34:58 +0100

    add: highlight tiles script (bug: tiny missadjustment)

---
+++ Assets/Scripts/CustomInspector/Button/InspectorButtonAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CustomInspector.Button
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class InspectorButtonAttribute : Attribute
+    {
+        public string ButtonName { get; }
+    
+        public InspectorButtonAttribute(string buttonName)
+        {
+            ButtonName = buttonName;
+        }
+    }
+}
