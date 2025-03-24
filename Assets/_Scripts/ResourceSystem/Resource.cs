54b748046286c574def32d89ddb06fca0887060a
Sevastian Bahynskyi <baginski.play@gmail.com>
2025-03-24 10:35:07 +0100

    implement resource system

---
+++ Assets/_Scripts/ResourceSystem/Resource.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace _Scripts.ResourceSystem
+{
+    // filename is default filename
+    // menuname is the unit menu structure it will appear under when request to create it
+    [CreateAssetMenu(fileName = "NewResource", menuName = "Game Resources/Resource", order = 1)]
+    public class Resource : ScriptableObject
+    {
+        public string resourceName;
+        public Sprite icon;
+        public int initialAmount;
+        public int maxAmount;
+    }
+}
---
+++ Assets/_Scripts/ResourceSystem/ResourceProducer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace _Scripts.ResourceSystem
+{
+    public class ResourceProducer : MonoBehaviour
+    {
+        [SerializeField] private ResourceController resourceController;
+        [SerializeField] private Resource resourceType;
+        [SerializeField] private int productionAmount;
+        [SerializeField] private float productionInterval;
+
+        private Coroutine productionCoroutine;
+
+        private void Start()
+        {
+            StartProduction();
+        }
+
+        public void StartProduction()
+        {
+            productionCoroutine ??= StartCoroutine(Produce());
+        }
+
+        public void StopProduction()
+        {
+            if (productionCoroutine == null) return;
+            StopCoroutine(productionCoroutine);
+            productionCoroutine = null;
+        }
+
+        private IEnumerator Produce()
+        {
+            while (true) // Keeps running until stopped
+            {
+                yield return new WaitForSeconds(productionInterval);
+                resourceController.AddResource(resourceType, productionAmount);
+                Debug.Log($"Resource: {resourceType.resourceName}. Produced {productionAmount}; Total {resourceController.GetResourceAmount(resourceType)}");
+            }
+        }
+    }
+}
