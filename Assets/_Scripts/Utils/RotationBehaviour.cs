eb0e6082216efddc0aec2ea5b3cdbe28ad696934
Sevastian Bahynskyi <baginski.play@gmail.com>
2025-03-28 20:21:25 +0100

    mage tower

---
+++ Assets/_Scripts/Utils/RotationBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+namespace _Scripts.Utils
+{
+    public class RotationBehaviour : MonoBehaviour
+    {
+        [SerializeField] private float speed = 3f;
+        [SerializeField] private Axis axis = Axis.X;
+        [SerializeField, Tooltip("Use local space for rotation")] private bool isLocal;
+
+        private void Update()
+        {
+            Vector3 rotationAxis = axis switch
+            {
+                Axis.X => Vector3.right,
+                Axis.Y => Vector3.up,
+                Axis.Z => Vector3.forward,
+                _ => Vector3.up
+            };
+
+            Space space = isLocal ? Space.Self : Space.World;
+            transform.Rotate(rotationAxis, speed * Time.deltaTime, space);
+        }
+    }
+
+    internal enum Axis
+    {
+        X,
+        Y,
+        Z
+    }
+}
