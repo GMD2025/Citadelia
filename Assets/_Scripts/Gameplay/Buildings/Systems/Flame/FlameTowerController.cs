488c2ef5457f54d05f0087ca81f18297c4178243
Sevastian Bahynskyi <baginski.play@gmail.com>
2025-04-07 11:13:06 +0200

    fix null in CheckAndFire

--- Assets/_Scripts/Gameplay/Buildings/Systems/Flame/FlameTowerController.cs
+++ Assets/_Scripts/Gameplay/Buildings/Systems/Flame/FlameTowerController.cs
@@ -34,9 +34,15 @@ namespace _Scripts.Gameplay.Buildings.Systems.Flame
         private void CheckAndFire()
         {
             Collider2D enemyCollider = Physics2D.OverlapCircle(transform.position, flameTowerData.detectionRadius, enemyLayer);
+            if (!enemyCollider)
+                return;
+            
             Transform flame = GetClosestFlame(enemyCollider.transform.position);
+            if (!flame)
+                return;
+            
             var flameProjectile = flame.GetComponent<FlameProjectile>();
-            if (!flameProjectile || !flame || !enemyCollider)
+            if (!flameProjectile)
                 return;
 
             bool hasFired = flameProjectile.FireFlame(enemyCollider.transform.position);
