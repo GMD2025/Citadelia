1233be423451f95ff1a0d9218eb6b8b81ee7017c
Sevastian Bahynskyi <baginski.play@gmail.com>
2025-04-23 02:06:23 +0200

    final boss mega merge

--- Assets/_Scripts/UI/BuildingPlacer.cs
+++ Assets/_Scripts/UI/BuildingPlacer.cs
@@ -1,6 +1,7 @@
 using _Scripts.ResourceSystem;
 using _Scripts.TilemapGrid;
 using _Scripts.UI.Buildings;
+using _Scripts.Utils;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using DG.Tweening;
@@ -9,7 +10,8 @@ namespace _Scripts.UI
 {
     public class BuildingPlacer : MonoBehaviour
     {
-        public Building Building { get; set; }
+        [SerializeField] private bool isTodayBlackFriday;
+        public BuildingData BuildingData { get; set; }
         public string BuildingSortingLayer { get; set; }
         private GameObject draggedBuilding;
 
@@ -38,36 +40,36 @@ namespace _Scripts.UI
 
         public void OnPointerDown()
         {
-            gridController.HighlightSize = Building.cellsize;
-            draggedBuilding = new GameObject(Building.name);
+            gridController.HighlightSize = BuildingData.cellsize;
+            draggedBuilding = new GameObject(BuildingData.name);
             draggedBuilding.transform.position = gridController.transform.position;
 
             SpriteRenderer spriteRenderer = draggedBuilding.AddComponent<SpriteRenderer>();
-            spriteRenderer.sprite = Building.sprite;
-            spriteRenderer.sortingLayerName = BuildingSortingLayer;
             spriteRenderer.color = new Color(1, 1, 1, 0.5f);
+            spriteRenderer.sprite = BuildingData.Sprite;
+            spriteRenderer.sortingLayerName = BuildingSortingLayer;
+            draggedBuilding.transform.localScale = BuildingPlacerUtility.GetLocalScale(BuildingData, gridGameObject);
         }
-
         public void OnPointerUp()
         {
             if (gridController.Selectable)
             {
-                bool richEnough = resourceService.SpendResources(Building.resources);
+                bool richEnough = resourceService.SpendResources(BuildingData.resources);
 
-                if (richEnough)
+                if ((isTodayBlackFriday || richEnough))
                 {
-                    GameObject newCell = Instantiate(draggedBuilding, gridGameObject.transform);
-                    var spriteRenderer = newCell.GetComponent<SpriteRenderer>();
-                    spriteRenderer.color = new Color(1, 1, 1, 1);
+                    GameObject newBuilding = Instantiate(BuildingData.buildingPrefab,
+                        draggedBuilding.transform.position, Quaternion.identity, gridGameObject.transform);
+                    BuildingPlacerUtility.AdjustSize(newBuilding, BuildingData, gridGameObject);
                 }
-                
+
                 gridController.SetTileAsOccupied();
             }
             else
             {
                 gridController.highlightParent.transform.DOShakePosition(1f, new Vector3(0.3f, 0, 0), 30);
             }
-            
+
             // Reset to one tile for PC to see the standard highlight on hover
             gridController.HighlightSize = new Vector2Int(1, 1);
             Destroy(draggedBuilding);
