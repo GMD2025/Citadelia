ea143f8ce147f1922bb5ebda258058f21cde1dc1
MichaelLeo-code <michael.leo.dk@gmail.com>
2025-05-01 00:33:04 +0200

    healthbar

--- Assets/_Scripts/Gameplay/Health/HealthController.cs
+++ Assets/_Scripts/Gameplay/Health/HealthController.cs
@@ -1,15 +1,16 @@
 ﻿using System;
+using System.Collections;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace _Scripts.Gameplay.Health
 {
     public class HealthController : MonoBehaviour
     {
         [SerializeField] private int startingHealth = 100;
-
         public int StartingHealth => startingHealth;
         public int Health => health;
-
         private int health;
 
         public event Action OnDied;
@@ -18,6 +19,26 @@ namespace _Scripts.Gameplay.Health
         private void Awake()
         {
             health = startingHealth;
+            TextMeshProUGUI text = GetComponentInChildren<TextMeshProUGUI>();
+            RectMask2D mask = GetComponentInChildren<RectMask2D>();
+
+            if (text != null)
+            {
+                AdaptTextToHealth(text);
+                OnHealthChange += () => AdaptTextToHealth(text);
+                OnHealthChange += () => AdaptMask(mask);
+            }
+
+            StartCoroutine(startTaking());
+        }
+
+        IEnumerator startTaking()
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(3f);
+                TakeDamage(10);
+            }
         }
 
         public void TakeDamage(int amount)
@@ -42,6 +63,16 @@ namespace _Scripts.Gameplay.Health
         {
             health = Mathf.Clamp(health + amount, 0, startingHealth);
             OnHealthChange?.Invoke();
+        }
+
+        private void AdaptTextToHealth(TextMeshProUGUI text)
+        {
+            text.text = $"{health.ToString()} / {startingHealth.ToString()}";
+        }
+        private void AdaptMask(RectMask2D mask)
+        {
+            float width = mask.rectTransform.rect.width;
+            mask.padding = new Vector4(0,0,(1 - health/startingHealth) * width, 0);
         }
     }
 }
